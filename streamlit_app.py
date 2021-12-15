fb42c56623f667bb628aa7f6f8e55cd87eb18819
Faith Tosin Olapade <45565979+Faithtosin@users.noreply.github.com>
2021-12-15 23:28:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,14 @@ import streamlit as st
 # import sys
 import os
 
-st.title("Test App 46")
+"""
+# Welcome to Streamlit v2!
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
+st.title("Test App 477")
 
 value = st.slider("Pick a number", 0, 10, 3)
 st.write(os.getcwd())
