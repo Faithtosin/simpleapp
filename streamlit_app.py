453a294ce04c149cef5ee40be3bdce8a64945f85
Faith Tosin Olapade <45565979+Faithtosin@users.noreply.github.com>
2022-08-02 10:49:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os
 
 """
-# Welcome to Streamlit v388348!!!!
+# Welcome to Streamlittttt v388348!!!!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
