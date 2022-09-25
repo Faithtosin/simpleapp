530b2f108c8d3347b31d0988535e1b145b0d5f8c
Faithtosin <tosin4rill@gmail.com>
2022-09-26 01:11:47 +0200

    line removal

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import os
 
 """
-# Welcome to Streamlit v19!!!!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Welcome to Streamlit v19!!!! :heart:
+
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
