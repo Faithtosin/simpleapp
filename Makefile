644d4150a7c6a37b0af7c0c73372d8ee3d21ad28
Faith Tosin Olapade <45565979+Faithtosin@users.noreply.github.com>
2022-09-26 22:34:32 +0200

    Ci/CD prep (#1)

---
+++ Makefile
@@ -0,0 +1,16 @@
+
+# Go parameters
+GOCMD=go
+GOBUILD=$(GOCMD) build
+GOCLEAN=$(GOCMD) clean
+GOGET=$(GOCMD) get
+REPO=public.ecr.aws/z1l0c6l7
+BINARY_NAME=simpleapp
+GIT_VERSION=$(shell git rev-parse HEAD)
+
+all: clean build
+
+docker-build:
+	docker build -t $(REPO)/$(BINARY_NAME):$(GIT_VERSION) .
+publish: docker-build
+	docker push $(REPO)/$(BINARY_NAME):$(GIT_VERSION)
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,19 @@ import streamlit as st
 import os
 
 """
-# Welcome to Streamlit v3800!!!!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Welcome to Streamlit v19!!!! :heart:
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 st.title("Test App 55577")
 
-value = st.slider("Pick a number", 0, 10, 3)
+value_1 = st.slider("Pick a number", 0, 10, 3)
+value_2 = st.slider("Pick a number", 30, 100, 32)
+addition = value_1 + value_2
 st.write(os.getcwd())
+st.write("The sum of your numbers is", addition)
 
-f = open("text", "a")
-f.write("something")
-f.close()
 #os.system("echo test >> text")
 
 # st.write(value)
